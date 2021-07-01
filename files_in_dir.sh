64212daa4fbe8e42956720d16755746023a5fc08
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2021-07-01 11:59:52 -0400

    Update files_in_dir.sh

--- files_in_dir.sh
+++ files_in_dir.sh
@@ -9,10 +9,8 @@ read -r DIR
 
 output "Searching for files"
 find "${DIR}" -type f -exec basename {} \; > file_info_temp1.txt
-awk -F _ '{ print $1 }' file_info_temp1.txt > file_info_temp6.txt
-find "${DIR}" -type f > file_info_temp2.txt
-
-paste -d, file_info_temp6.txt file_info_temp2.txt > file_info_temp3.txt
+awk -F _ '{ print $1 }' file_info_temp1.txt > file_info_temp2.txt
+find "${DIR}" -type f > file_info_temp3.txt
 
 while read -r line; do
     if [[ $line == *"R1"* ]]; then
@@ -24,9 +22,9 @@ while read -r line; do
     fi
 done < file_info_temp1.txt
 
-paste -d, file_info_temp3.txt file_info_temp4.txt | sort -n > file_info_output.txt
+paste -d, file_info_temp2.txt file_info_temp3.txt file_info_temp4.txt | sort -n > file_info_output.txt
 output "Cleaning up"
 
-rm file_info_temp{1,2,3,4,6}.txt
+rm file_info_temp{1,2,3,4}.txt
 
 output "Done."
