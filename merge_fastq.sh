4288b19ca75d753812c2e89af6d080fd382a2067
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2021-07-29 13:01:55 -0400

    Update merge_fastq.sh

--- merge_fastq.sh
+++ merge_fastq.sh
@@ -3,6 +3,6 @@
 echo "Please enter the main directory:"
 read MAINDIR
 
-find $MAINDIR -type f -name *.fastq -exec cat {} >> result.fastq \;
+find $MAINDIR -type f -name '*.fastq' -exec cat {} >> result.fastq \;
 
 echo "All done. Have a good one."
