081b78b18f67bb73b076304ef97a666624c4984d
ajeyrgowda <72326100+ajeyrgowda@users.noreply.github.com>
2021-07-29 18:33:13 -0400

    Update merge_fastq.sh

--- merge_fastq.sh
+++ merge_fastq.sh
@@ -7,7 +7,7 @@ echo "Please enter the extension to cat and merge (ex. .fastq, .fasta):"
 read EXTENSION
 
 if [ -f result"${EXTENSION}" ]; then
-    echo "result""${EXTENSION}"" file already exist. Please make a back up of it and remove it from the current directory before running the script."
+    echo "result""${EXTENSION}"" file already exists. Please make a back up of it and remove it from the current directory before running the script."
     exit 1
 fi
 
