d9cda1251dd446198bf9d4552bfcc670bd67860d
ajeyrgowda <72326100+ajeyrgowda@users.noreply.github.com>
2021-07-29 18:18:39 -0400

    Update merge_fastq.sh

--- merge_fastq.sh
+++ merge_fastq.sh
@@ -6,6 +6,11 @@ read MAINDIR
 echo "Please enter the extension to cat and merge (ex. .fastq, .fasta):"
 read EXTENSION
 
+if [ -f result"${EXTENSION}" ]; then
+    echo "result""${EXTENSION}"" file already exist. Please make a back up of it and remove it from the current directory before running the script."
+    exit 1
+fi
+
 find "$MAINDIR" -type f -name "*${EXTENSION}" -exec cat {} >> result"${EXTENSION}" \;
 
 echo "All done. Have a good one."
