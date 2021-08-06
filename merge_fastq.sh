df2aa10f2cd61ae8a93159fd077293a6e3671c60
tommytran732 <57488583+tommytran732@users.noreply.github.com>
2021-08-06 19:17:33 -0400

    Create merge_fastq.sh

---
+++ merge_fastq.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+
+output "Please enter the main directory:"
+read MAINDIR
+
+if [ -f result.fasta ]; then
+    output "result.fasta file already exist. Please make a back up of it and remove it from the current directory before running the script."
+    exit 1
+fi
+
+find "$MAINDIR" -type f -name "*.fasta"\; > files2scan.txt
+
+for file in `cat files2scan.txt`; do
+    echo ">$(basename ${file})" > result.fasta
+    sed '1d' ${file} > result.fasta
+done
+
+output "Cleaning up"
+rm files2scan.txt
+
+output "All done. Have a good one."
