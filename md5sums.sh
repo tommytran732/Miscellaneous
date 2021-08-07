994270cc33cd33f9ae45682f4e9f14a7a7d0e06c
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2021-08-07 04:16:54 -0400

    Update md5sums.sh

--- md5sums.sh
+++ md5sums.sh
@@ -4,7 +4,7 @@ output(){
     echo -e '\e[36m'$1'\e[0m';
 }
 
-if [[ -f file_info_output.txt ]]; then
+if [[ -f md5sums.txt ]]; then
     output "Temporary files found! Did you already run the script?"
     output "Exiting"
     exit 1
@@ -14,6 +14,6 @@ output "Enter the directory to search: "
 read -r DIR
 
 output "Searching for files"
-find "${DIR}" -type f -exec md5sum {} \; > md5sums.txt
+find "${DIR}" -type f -exec md5sum {} \; >> md5sums.txt
 
 output "Done."
