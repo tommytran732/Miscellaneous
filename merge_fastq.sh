217399f31e1049a9e8be9c6d579616fb84c1a359
tommytran732 <57488583+tommytran732@users.noreply.github.com>
2021-08-06 21:26:59 -0400

    Create merge_fastq.sh

---
+++ merge_fastq.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+
+output "Please enter the main directory:"
+read -r MAINDIR
+
+for DIR in $(find $MAINDIR -type d); do
+    find $DIR -type f -name "*fastq.gz" -exec gunzip -k {} \;
+    cat $DIR/*.fasta > $(basename ${DIR}).fasta
+    chattr +i $(basename ${DIR}).fasta
+    rm -f $DIR/*.fasta >/dev/null
+    chattr -i $DIR/$(basename ${DIR}).fasta
+done
+
+output "Done"
