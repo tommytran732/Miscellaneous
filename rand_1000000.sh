716528c840cd74a0ae709ff6a0f71bc34d862fd8
tommytran732 <57488583+tommytran732@users.noreply.github.com>
2021-08-11 14:30:19 -0400

    Create rand_1000000.sh

---
+++ rand_1000000.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+
+output "Please enter the main directory:"
+read -r MAINDIR
+
+if [ -d "$MAINDIR"/RESULT ]; then
+    output "RESULT folder already exists. Please make a back up of it and remove it from the current directory before running the script."
+    exit 1
+fi
+
+output "Enter the number of reads in subsample"
+read -r SIZE
+
+for file in $(find "$MAINDIR" -type f -name "*.fastq"); do
+    seqtk sample -s"${SIZE}" "${file}" 1000000 > "$MAINDIR"/RESULT/$(basename "${file}").fastq
+done
+
+output "Done"
