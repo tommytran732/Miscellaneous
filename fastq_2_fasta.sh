9dd639f4f346f85cde525ec1f7be726eea4b12f1
tommytran732 <57488583+tommytran732@users.noreply.github.com>
2021-08-12 10:53:37 -0400

    Update fastq_2_fasta.sh

--- fastq_2_fasta.sh
+++ fastq_2_fasta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Please enter the main directory:"
-read MAINDIR
+read -r MAINDIR
 
 if [ -d "$MAINDIR"/RESULT ]; then
     echo "fastq_merged_2_fasta.fasta file already exists. Please make a back up of it and remove it from the current directory before running the script."
@@ -10,6 +10,8 @@ fi
 
 mkdir "$MAINDIR"/RESULT
 
-find "$MAINDIR" -type f -name "*.fastq" -exec sed -n '1~4s/^@/>/p;2~4p' {} \; | tee "$MAINDIR"/RESULT/$(basename {}).fasta
+for file in $(find "$MAINDIR" -type f -name "*.fastq"); do
+    sed -n '1~4s/^@/>/p;2~4p' "${file}" | tee "$MAINDIR"/RESULT/$(basename "${file}" | awk -F .).fasta
+done
 
 echo "All done. Have a good one."
