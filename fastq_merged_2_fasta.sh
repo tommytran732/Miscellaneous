2db05c07140ef2735a5d2a6e0b19fe26daffa075
TommyTran732 <tommytran732@protonmail.com>
2021-08-14 21:09:45 -0400

    Update

--- fastq_merged_2_fasta.sh
+++ fastq_merged_2_fasta.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo "Please enter the main directory:"
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+
+output "Please enter the main directory:"
 read -r MAINDIR
 
 if [ -f fastq_merged_2_fasta.fasta ]; then
-    echo "fastq_merged_2_fasta.fasta file already exists. Please make a back up of it and remove it from the current directory before running the script."
+    output "fastq_merged_2_fasta.fasta file already exists. Please make a back up of it and remove it from the current directory before running the script."
     exit 1
 fi
 
 find "$MAINDIR" -type f -name "*.fastq" -exec sed -n '1~4s/^@/>/p;2~4p' {} \; >> fastq_merged_2_fasta.fasta
 
-echo "All done. Have a good one."
+output "All done. Have a good one."
