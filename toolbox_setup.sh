41c1236d82bb9bc2f64c2a03eac8c769810ef4b4
TommyTran732 <tommytran732@protonmail.com>
2021-08-15 12:47:03 -0400

    Update

--- toolbox_setup.sh
+++ toolbox_setup.sh
@@ -3,6 +3,7 @@
 #Script to setup a toolbox container with development tools
 
 sudo dnf upgrade -y
+sudo dnf autoremove -y
 sudo dnf install neofetch butane net-tools java-latest-openjdk speedtest-cli -y
 
 #Pentest tools
