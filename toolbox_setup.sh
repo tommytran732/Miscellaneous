16f73e47cf6640c261b33693d451029507105a9c
TommyTran732 <tommytran732@protonmail.com>
2021-08-15 12:39:02 -0400

    Update

---
+++ toolbox_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Script to setup a toolbox container with development tools
+
+sudo dnf upgrade -y
+sudo dnf install neofetch butane net-tools java-latest-openjdk speedtest-cli -y
+
+#Pentest tools
+sudo dnf install nmap hydra wireshark -y
